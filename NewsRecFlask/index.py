4e6e5e62a3409d308c2883799bf44e4fb407d00b
Alexander Fang <alex.s.fang@gmail.com>
2018-11-26 15:10:03 -0600

    progress on Flask

--- NewsRecFlask/index.py
+++ NewsRecFlask/index.py
@@ -1,8 +1,14 @@
 from flask import Flask, redirect, url_for, render_template
+from python_twitter_test import *
 
 app = Flask(__name__)
+handle = ""
 
 @app.route('/')
 def index():
 	return render_template('index.html')
+
+@app.route('/results', methods=['GET', 'POST'])
+def results():
+	return render_template('results.html')
 
