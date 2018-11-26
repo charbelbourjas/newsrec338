50ddfb9c8d1d1762d7c70a4722b0b2b3eb54c3bd
David Nelson <davidnelson2019@u.northwestern.edu>
2018-11-26 14:51:34 -0600

    input output method

--- public/scripts/Article_Recommender.py
+++ public/scripts/Article_Recommender.py
@@ -23,6 +23,15 @@ def process_data(filepath):
 	    url_data.append(i['article_url'])
 	return (data['user_tweets'], text_data, url_data)
 
+def process_data2(data_input):
+	data = data_input
+	text_data = list()
+	url_data = list()
+	for i in data['article_pooltweets']:
+	    text_data.append(i['person_tweets'])
+	    url_data.append(i['article_url'])
+	return (data['user_tweets'], text_data, url_data)
+
 
 def get_matches(urls, indices):
 	matches = list()
@@ -45,8 +54,9 @@ def get_keywords(urls):
 		ls.append(d)
 	return ls
 
-def main():
-	data = process_data('ex.txt')
+
+def recommend(data):
+	data = process_data2('ex.txt')
 	text_data = data[1]
 	url_data = data[2]
 	user_data = data[0]
@@ -58,8 +68,24 @@ def main():
 	distances, indices = clf.kneighbors(X_train_tfidf[0])
 	output = dict()
 	output['data'] = get_keywords(get_matches(url_data, indices))
-	with open('recommended_articles.txt', 'w') as outfile:  
-		json.dump(output, outfile)
+	return output
+
+def main():
+	return 1
+	# data = process_data('ex.txt')
+	# text_data = data[1]
+	# url_data = data[2]
+	# user_data = data[0]
+	# count_vect = CountVectorizer()
+	# tfidf_transformer = TfidfTransformer()
+	# X_train_counts = count_vect.fit_transform([user_data]+text_data)
+	# X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
+	# clf = NearestNeighbors(num_neighbors, algorithm='auto').fit(X_train_tfidf)
+	# distances, indices = clf.kneighbors(X_train_tfidf[0])
+	# output = dict()
+	# output['data'] = get_keywords(get_matches(url_data, indices))
+	# with open('recommended_articles.txt', 'w') as outfile:  
+	# 	json.dump(output, outfile)
 
 
 if __name__ == '__main__':
